c85ce5f281de3c66f1e97c0b9de55aed8bffe9eb
nicofercavv-dev <nicoleferreira281@gmail.com>
2022-09-01 19:45:02 -0300

    consegui animar essa porra

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,21 +4,39 @@ let botao2 = document.querySelector(".dois").onclick = function() { recebeNota(t
 let botao3 = document.querySelector(".tres").onclick = function() { recebeNota(this); };
 let botao4 = document.querySelector(".quatro").onclick = function() { recebeNota(this); };
 let botao5 = document.querySelector(".cinco").onclick = function() { recebeNota(this); };
+let main = document.querySelector("main");
 
 function recebeNota(botao) {
     nota = botao.value;
     console.log(nota);
 }
 
-let buttonSubmit = document.querySelector(".submit").onclick = function() {  };
+let buttonSubmit = document.querySelector(".submit").onclick = function() { confirmaNota(); };
 
 function confirmaNota() {
-    let body = document.getElementsByTagName("body");
-    let imagemThanks = document.createElement("img");
-    imagemThanks.setAttribute('src="assets/images/illustration-thank-you.svg"');
-    imagemThanks.setAttribute('alt="Thank You Illustration"');
-    imagemThanks.classList.add("submit");
-    body.appendChild(imagemThanks);
-    setInterval(3000);
-    document.location.reload(true);
+
+    let imagem = document.createElement("img");
+    let span = document.createElement("span");
+    let div = document.createElement("div");
+
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
+
+    main.classList.add("after-submit");
+
+    imagem.setAttribute("src", "assets/images/illustration-thank-you.svg");
+    imagem.setAttribute("alt", "Smartphone Illustration");
+    span.innerText = `You selected ${nota} out of 5`;
+    div.innerHTML = "<h1>Thank you!</h1>\n<p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch! </p>";
+
+    main.appendChild(imagem);
+    main.appendChild(span);
+    main.appendChild(div);
+
+    main.classList.toggle("animar");
+
+    const INTERVALO_PARA_RECARREGAR_PAGINA = setTimeout(() => {
+        document.location.reload(true);
+    }, 5000);
 }
